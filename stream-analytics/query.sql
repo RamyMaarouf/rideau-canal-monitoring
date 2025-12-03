c30e737d0eee4ca2d71175cc1dfb66bb5960e09e
Ramy Maarouf <ramymaarouf255@gmail.com>
2025-12-02 23:05:01 -0500

    Update query.sql

--- stream-analytics/query.sql
+++ stream-analytics/query.sql
@@ -1,1 +1,19 @@
 
+SELECT
+    System.Timestamp() AS WindowEndTime,
+    'SensorAggregations' AS documentId, -- Required for Cosmos DB
+    Location,
+    AVG(IceThickness) AS AvgIceThickness_cm,
+    AVG(SurfaceTemperature) AS AvgSurfaceTemp_C,
+    CASE
+        WHEN AVG(IceThickness) >= 30.0 THEN 'Safe'
+        WHEN AVG(IceThickness) >= 20.0 AND AVG(IceThickness) < 30.0 THEN 'Caution'
+        ELSE 'Unsafe'
+    END AS SafetyStatus
+INTO
+    [CosmosDB-Output]
+FROM
+    [IoTHub-Input]
+GROUP BY
+    Location,
+    TUMBLINGWINDOW(mi, 5) -- Aggregates data every 5 minutes
